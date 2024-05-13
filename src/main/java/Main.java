5d41c7d71ec0d4784d8af950f6108b2ee87f33b6
Cauan Josias Moreira <168428906+CauanJMoreira@users.noreply.github.com>
2024-05-13 15:21:26 +0000

    13 de Maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.plintln("+++++++++++++")
-    System.out.println("");
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+    System.out.println("+banda: Nirvana+"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+    System.out.println("-Álbum: Nevermind-"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-Ano de lançamento: 24 de Setembro de 1991-"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+    System.out.println(" O álbum contém 13 faixas, sendo as principais: "); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+    System.out.println("1. Smells Like Teen Spirit"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("2. In Bloom"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+    System.out.println("3. Come as You Are"); 
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
   }
 
 }
